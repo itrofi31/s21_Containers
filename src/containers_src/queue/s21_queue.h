d21834e7ff8a77b7c44b07933fea0e0da479256b
Ivan <grapefrr@student.school-21.ru>
2024-05-18 13:04:12 +0300

    refactor: put dequeue to another file

--- src/containers_src/queue/s21_queue.h
+++ src/containers_src/queue/s21_queue.h
@@ -1,5 +1,5 @@
-#ifndef S21_CONTAINERS_DEQUEUE_H_
-#define S21_CONTAINERS_DEQUEUE_H_
+#ifndef S21_CONTAINERS_QUEUE_H_
+#define S21_CONTAINERS_QUEUE_H_
 
 #include <iostream>
 #include <utility>
@@ -7,46 +7,8 @@
 namespace s21 {
 
 template <typename T>
-class dequeue {
- protected:
-  struct Node {
-    T data;
-    Node* next;
-    Node* prev;
-    Node(T data) : data(data), next(nullptr), prev(nullptr) {}
-  };
-
-  Node* head_;
-  Node* tail_;
-  std::size_t size_;
-
- public:
-  // constructors
-  dequeue();
-  dequeue(const dequeue<T>& other);
-  dequeue(dequeue<T>&& other);
-  dequeue(std::initializer_list<T> const& items);
-  ~dequeue();
-
-  // methods
-  void push_front(const T& value);
-  void push_back(const T& value);
-  void pop_front();
-  void pop_back();
-
-  // operators
-  dequeue<T>& operator=(const dequeue<T>& other);
-  dequeue<T>& operator=(dequeue<T>&& other);
-
-  const T& front();
-  const T& back();
-  bool empty();
-  void swap(dequeue<T>& other);
-  void clear();
-
-  std::size_t size();
-};
+class queue {};
 
 }  // namespace s21
 
-#endif /* S21_CONTAINERS_DEQUEUE_H_ */
+#endif /* S21_CONTAINERS_QUEUE_H_ */
