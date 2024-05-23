e588334c7a350ae7fd73f3e451f468ff3c86cb8e
Ivan <triv99@mail.ru>
2024-05-23 18:52:20 +0300

    feat: add list::splice

--- src/test_src/test_list.cc
+++ src/test_src/test_list.cc
@@ -4,31 +4,55 @@
 
 class ListSampleTest : public testing::Test {};
 
-// TEST_F(ListSampleTest, test1) {
-//   s21::list<int> list;
-//   std::list<int> slist;
-//   EXPECT_EQ(list.max_size(), slist.max_size());
-// }
-TEST(test, test1) {
-  // s21::list<int> list = {1, 3, 2, 4};
-  s21::list<int> slist = {1, 3, 2, 4};
+TEST(List, max_size) {
+  s21::list<int> list;
+  std::list<int> slist;
+  EXPECT_EQ(list.max_size(), slist.max_size() / 2);
+}
+
+TEST(List, erase) {
+  s21::list<int> list{1, 2, 3};
+  s21::list<int>::iterator it = list.begin();
+  it++;
+  list.erase(it);
+  EXPECT_EQ(list.size(), size_t(2));
+  EXPECT_EQ(list.back(), 3);
+}
+
+TEST(List, reverse) {
+  s21::list<int> list = {1, 3, 2};
+  std::list<int> slist = {2, 3, 1};
   s21::list<int>::iterator it;
-  // slist.merge(list);
-  for (it = slist.begin(); it != slist.end(); it++) std::cout << " " << *it;
-  std::cout << std::endl;
-  // list.reverse();
-  // slist.reverse();
+  list.reverse();
 
-  // list.reverse();
+  EXPECT_EQ(list.front(), slist.front());
+  list.pop_front();
+  slist.pop_front();
+  EXPECT_EQ(list.front(), slist.front());
+}
 
-  // for (size_t i = 0; i < 4; i++) {
-  //   std::cout << list.front() << " - ";
-  //   list.pop_front();
-  // }
-  // std::cout << '\n';
-  // for (size_t i = 0; i < slist.size(); i++) {
-  //   std::cout << "slist" << slist.front() << ' ';
-  //   slist.pop_front();
-  // }
-  // ASSERT_EQ(list.reverse() slist.reverse());
+TEST(List, unique) {
+  s21::list<int> list = {1, 1, 2, 2, 2, 3};
+  s21::list<int> slist = {1, 2, 3};
+  list.unique();
+  s21::list<int>::iterator it = list.begin();
+  s21::list<int>::iterator sit = slist.begin();
+
+  EXPECT_EQ(*it++, *sit++);
+  EXPECT_EQ(*it++, *sit++);
+  EXPECT_EQ(*it, *sit);
+}
+
+TEST(List, splice) {
+  s21::list<int> list = {1, 5, 6};
+  s21::list<int> slist = {2, 3, 4};
+  s21::list<int>::iterator it = list.begin();
+  it++;
+  list.splice(it, slist);
+  s21::list<int> expected = {1, 2, 3, 4, 5, 6};
+  it = list.begin();
+  for (int i = 1; i < 7; ++i, ++it) {
+    EXPECT_EQ(*it, i);
+  }
+  EXPECT_TRUE(slist.empty());
 }
