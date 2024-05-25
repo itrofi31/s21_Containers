dcd4ec59a775a6bb9ee92ec19c650a5a81462ef3
Ivan <triv99@mail.ru>
2024-05-25 10:48:11 +0300

    Stacl tests

--- src/test_src/test_stack.cc
+++ src/test_src/test_stack.cc
@@ -1,5 +1,74 @@
 #include "test.h"
 
-class StackSampleTest : public testing::Test {};
+TEST(Stack, constructor_1) {
+  s21::stack<int> s;
+  EXPECT_TRUE(s.empty());
+}
 
-TEST_F(StackSampleTest, test1) {}
+TEST(Stack, constructor_2) {
+  s21::stack<int> s = {1, 2, 3, 4, 5};
+  s21::stack<int> s2 = std::move(s);
+  EXPECT_TRUE(s2.size() == 5);
+  EXPECT_TRUE(s.empty());
+}
+
+TEST(Stack, constructor_3) {
+  s21::stack<int> s = {1, 2, 3, 4, 5};
+  s21::stack<int> s2 = s;
+  EXPECT_TRUE(s2.size() == 5);
+  EXPECT_TRUE(s.top() == s2.top());
+}
+
+TEST(Stack, operator_1) {
+  s21::stack<int> s = {1, 2, 3, 4, 5};
+  s21::stack<int> s2;
+  s2 = s;
+  EXPECT_EQ(s.size(), s2.size());
+  EXPECT_EQ(s.top(), s2.top());
+}
+
+TEST(Stack, operator_2) {
+  s21::stack<int> s = {1, 2, 3, 4, 5};
+  s21::stack<int> s2;
+  s2 = std::move(s);
+  EXPECT_TRUE(s2.size() == 5);
+  EXPECT_TRUE(s.empty());
+}
+
+TEST(Stack, push) {
+  s21::stack<int> s;
+  std::stack<int> std_s;
+  for (int i = 0; i < 3; i++) {
+    s.push(i);
+    std_s.push(i);
+    EXPECT_EQ(s.top(), std_s.top());
+  }
+}
+
+TEST(Stack, pop) {
+  s21::stack<int> s;
+  std::stack<int> std_s;
+  for (int i = 0; i < 3; i++) {
+    s.push(i);
+    std_s.push(i);
+  }
+  s.pop();
+  std_s.pop();
+  EXPECT_EQ(s.top(), std_s.top());
+  EXPECT_TRUE(s.size() == 2);
+}
+
+TEST(Stack, swap) {
+  s21::stack<int> s = {1, 2, 3, 4, 5};
+  s21::stack<int> s2 = {21};
+  s.swap(s2);
+  EXPECT_TRUE(s2.size() == 5);
+  EXPECT_TRUE(s.size() == 1);
+  EXPECT_EQ(s.top(), 21);
+}
+
+TEST(Stack, insert_many) {
+  s21::stack<int> s;
+  s.insert_many_front(1, 2, 3, 4, 5);
+  EXPECT_TRUE(s.size() == 5);
+}
